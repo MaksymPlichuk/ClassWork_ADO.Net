c1fbe4321db7ad9f3c49993be7018cf3ad41fdf6
MaskymPlichuk <pilipm449@gmail.com>
2025-09-10 17:53:27 +0300

    Tasks 1-5  to/do

---
+++ 02_CRUD_Interface/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CRUD_Interface
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string BuyerID { get; set; }
+        public int SellerID { get; set; }
+        public int Price { get; set; }
+        public DateTime SaleDate { get; set; }
+    }
+}
---
+++ 02_CRUD_Interface/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace _02_CRUD_Interface
+{
+    class SalesDb: IDisposable
+    {
+        private SqlConnection sqlConnection;
+        public SalesDb(string connection)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connection);
+            sqlConnection.Open();
+        }
+        public void Create_As_Insert(Product product)
+        {
+            string cmdText = $@"INSERT INTO Products
+                              VALUES ({product.BuyerID}, 
+                                      {product.SellerID},
+                                       {product.Price}, 
+                                       '{product.SaleDate}')";
+
+            SqlCommand command = new SqlCommand(cmdText, sqlConnection);
+            command.CommandTimeout = 5;
+
+            int rows = command.ExecuteNonQuery();
+            Console.WriteLine(rows + " rows affected!");
+        }
+
+        public void Dispose()
+        {
+            sqlConnection.Close();
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source = (localDB)\MSSQLLocalDb; 
+                                        Initial Catalog = Sales;
+                                        Integrated Security = true; TrustServerCertificate=True;";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            Console.WriteLine("Connected success!");
+
+            ShowBuyersInfo(sqlConnection);
+            ShowSellerrsInfo(sqlConnection);
+            ShowSaleInfo(sqlConnection);
+
+            sqlConnection.Close();
+        }
+    }
+}
