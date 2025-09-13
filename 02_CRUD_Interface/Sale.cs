3103f03d580ef09b6a6449f0b78080f39bbf3791
Maksym <pilipm449@gmail.com>
2025-09-13 13:29:37 +0300

    Tasks 1-5 done HW

--- 02_CRUD_Interface/Sale.cs
+++ 02_CRUD_Interface/Sale.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace _02_CRUD_Interface
 {
-    internal class Product
+    internal class Sale
     {
         public int Id { get; set; }
-        public string BuyerID { get; set; }
+        public int BuyerID { get; set; }
         public int SellerID { get; set; }
         public int Price { get; set; }
         public DateTime SaleDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{BuyerID,5} {SellerID,5} {Price,10} {SaleDate,15}";
+        }
     }
 }
