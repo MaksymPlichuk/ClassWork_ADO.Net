55f7fe2a4fba57ee55443d8143acc8ab98f5b9cb
Maksym <pilipm449@gmail.com>
2025-09-14 14:27:51 +0300

    Prevented SQL injection

---
+++ 03_SQLInjectionDataAccessLayer/Sale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SQLInjectionDataAccessLayer
+{
+    internal class Sale
+    {
+        public int Id { get; set; }
+        public int BuyerID { get; set; }
+        public int SellerID { get; set; }
+        public int Price { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{BuyerID,5} {SellerID,5} {Price,10} {SaleDate,15}";
+        }
+    }
+}
